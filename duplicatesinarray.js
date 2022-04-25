84d3fd400bdbb974de961ccdce45902c18926527
Koduri-Bhavitha <103644033+Koduri-Bhavitha@users.noreply.github.com>
2022-04-25 17:49:33 +0530

    Add files via upload

---
+++ duplicatesinarray.js
@@ -0,0 +1,5 @@
+var Days=['monday','tuesday','wednesday','thursday','friday'];
+Days.indexOf('wednesday');
+Days.indexOf('saturday')
+console.log(Days.indexOf('wednesday'));
+console.log(Days.indexOf('saturday'));
